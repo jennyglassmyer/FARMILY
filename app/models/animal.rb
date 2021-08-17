8721463b91dee95cf7a4743f7167c9a4478cff51
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-17 11:42:12 +0200

    create animals controller, routes, and actions

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  validates :species, inclusion: { in: ["Horse", "Cow", "Pig", "Dog", "Chicken", "Cat", "Goat", "Sheep", "Duck", "Goose"] }
 end
