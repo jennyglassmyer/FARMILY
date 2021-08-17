fb85a4af1fdd5e8a266717596f41dfa4166aea02
Philipp Nordmeyer <p.nordmeyer@zeppelin-university.net>
2021-08-17 12:39:31 +0200

    show page and validations done for a new animal done

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,13 @@ class Animal < ApplicationRecord
   has_many :bookings
   belongs_to :user
   validates :species, inclusion: { in: ["Horse", "Cow", "Pig", "Dog", "Chicken", "Cat", "Goat", "Sheep", "Duck", "Goose"] }
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :gender, presence: true
+  validates :personality, presence: true
+  validates :description, presence: true
+  validates :care_level, presence: true
+  validates :age, presence: true
+  validates :price_per_day, presence: true
+  validates :care_instructions, presence: true
 end
