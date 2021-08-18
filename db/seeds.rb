89008c0afe18cc89de73114266b63e6543a7ba76
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-18 12:01:49 +0200

    fix seed id prob

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+require "pry-byebug"
 require "open-uri"
 Animal.destroy_all
 User.destroy_all
 
-user_1 = User.create!(
+user_1 = User.create(
   first_name: "Marie",
   last_name: "Hasselberg",
   email: "marie@marie.com",
@@ -13,7 +14,7 @@ user_1 = User.create!(
   password: "123456"
 )
 
-user_2 = User.create!(
+user_2 = User.create(
   first_name: "Philip",
   last_name: "Nordy",
   email: "nordy@nordy.com",
@@ -24,7 +25,7 @@ user_2 = User.create!(
   password: "123456"
 )
 
-user_3 = User.create!(
+user_3 = User.create(
   first_name: "Otmar",
   last_name: "Jenner",
   email: "otmar@otmar.com",
@@ -35,7 +36,7 @@ user_3 = User.create!(
   password: "123456"
 )
 
-user_4 = User.create!(
+user_4 = User.create(
   first_name: "Jenny",
   last_name: "Glassmyer",
   email: "jennifer@gmail.com",
@@ -46,7 +47,7 @@ user_4 = User.create!(
   password: "123456"
 )
 
-animal1 = Animal.create!(
+animal1 = Animal.create(
   name: "Rudi",
   species: "Horse",
   breed: "Trakehner",
@@ -57,7 +58,7 @@ animal1 = Animal.create!(
   age: 5,
   price_per_day: 150,
   care_instructions: "Need fresh hey every day, 10 litres of water, no alcohol, please, had a former addiction.",
-  user: user_1
+  user_id: user_1.id
   )
 
   file = URI.open('https://images.unsplash.com/photo-1622037088021-26eb84ef2389?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
@@ -65,17 +66,17 @@ animal1 = Animal.create!(
   animal1.photos.attach(io: file, filename: 'horse.jpg', content_type: 'image/jpg')
 
 
-  animal2 = Animal.create!(
-  name: "Boris",
-  species: "Pig",
-  gender: "Male",
-  personality: "Funny",
-  description: "A mate who rolls with joy and is very clean.",
-  care_level: "Medium",
-  age: 7,
-  price_per_day: 80,
-  care_instructions: "Needs 10 Kilos of food per day, loves fried vegetables and a lot of hugging. No sweets please.",
-  user: user_2
+  animal2 = Animal.create(
+    name: "Boris",
+    species: "Pig",
+    gender: "Male",
+    personality: "Funny",
+    description: "A mate who rolls with joy and is very clean.",
+    care_level: "Medium",
+    age: 7,
+    price_per_day: 80,
+    care_instructions: "Needs 10 Kilos of food per day, loves fried vegetables and a lot of hugging. No sweets please.",
+    user_id: user_1.id
   )
 
   file = URI.open('https://images.unsplash.com/photo-1627930738037-9127714a9434?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
@@ -84,7 +85,7 @@ animal1 = Animal.create!(
   animal2.photos.attach(io: file, filename: 'pig.jpg', content_type: 'image/jpg')
 
 
-  animal3 = Animal.create!(
+  animal3 = Animal.create(
   name: "Isabelle",
   species: "Cat",
   breed: "Maine Coon",
@@ -95,7 +96,7 @@ animal1 = Animal.create!(
   age: 3,
   price_per_day: 80,
   care_instructions: "Feed twice daily and lots of pets.",
-  user: user_3
+  user_id: user_3.id
   )
 
   file = URI.open('https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')
@@ -103,7 +104,7 @@ animal1 = Animal.create!(
   animal3.photos.attach(io: file, filename: 'cat.jpg', content_type: 'image/jpg')
 
 
-  animal4 = Animal.create!(
+  animal4 = Animal.create(
   name: "Ella",
   species: "Cow",
   breed: "Jersey",
@@ -114,7 +115,7 @@ animal1 = Animal.create!(
   age: 3,
   price_per_day: 50,
   care_instructions: "Needs a large pasture and plenty of grass.",
-  user: user_4
+  user_id: user_4.id
   )
 
   file = URI.open('https://images.unsplash.com/photo-1500595046743-cd271d694d30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80')
