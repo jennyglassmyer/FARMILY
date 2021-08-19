1cad7450d130306282be65e2dd94cb49cb7d400b
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-19 16:44:07 +0200

    implement dashboard and booking accept and decline functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_090928) do
+ActiveRecord::Schema.define(version: 2021_08_19_111214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_090928) do
     t.float "total_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["animal_id"], name: "index_bookings_on_animal_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
