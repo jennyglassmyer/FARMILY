e875bd5ec96963d35f3d718a2dddae33f4c73dfb
Philipp Nordmeyer <p.nordmeyer@zeppelin-university.net>
2021-08-19 12:58:18 +0200

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:index, :show, :new, :create]
   end
   resources :bookings, only: :destroy
+  resources :dashboard, only: :index
 end
